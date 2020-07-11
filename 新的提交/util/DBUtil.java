dba675e2b162ab730b438feab0f247188ce24d78
xqdxzs <65845921+xqdxzs@users.noreply.github.com>
2020-07-11 08:35:23 +0800

    Add files via upload

---
+++ 新的提交/util/DBUtil.java
@@ -0,0 +1,21 @@
+package cn.edu.zucc.ttcp.util;
+
+import java.sql.Connection;
+
+public class DBUtil {
+		private static final String jdbcUrl="jdbc:mysql://localhost:3306/444";
+		private static final String dbUser="root";
+		private static final String dbPwd="123456";
+		static{
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		public static Connection getConnection() throws java.sql.SQLException{
+			return java.sql.DriverManager.getConnection(jdbcUrl, dbUser, dbPwd);
+		}
+	
+}
